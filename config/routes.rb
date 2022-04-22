03969416839d3929651da8a6ae310a10746a6ad1
M Shiddiq F <msf31@student.gunadarma.ac.id>
2022-04-22 16:06:55 +0700

    Change Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :orders, :order_details, :menus, :category_menus, :categories
 end
